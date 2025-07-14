07c8dbdc504a514f336609f3eeae238d472b544e
Sai Srinivas Lakkakula <saisrinu.lakkakula@gmail.com>
2025-07-14 00:36:56 -0700

    Ch 01 - Temperature example

--- main.py
+++ main.py
@@ -5,14 +5,10 @@ from service import chat
 if __name__ == '__main__':
     load_dotenv()
     messages = []
-    system = """
-    you are a good programmer. your task is to generate code that is efficient,
-    performant, well organized. You have to be precise and not add too much verbose comments in the code.
-    """
 
     while True:
         user_text = input("YOU:")
         utils.add_user_message(messages, user_text)
-        ai_response = chat.chat_service(messages, system)
+        ai_response = chat.chat_service(messages,None, 0.0)
         print(f"AI: {ai_response}")
         utils.add_assistant_message(messages, ai_response)
--- service/chat.py
+++ service/chat.py
@@ -1,13 +1,48 @@
 from anthropic import Anthropic
 
+'''
+NOTES:
+System: a system prompt can be passed to the chat function if necessary. Example coud be, 
+you are an intelligent math tutor helping a 5th grader kid. Do not give solution right away, instead help 
+them to understand and encourage them into solving the problem on their own.
 
-def chat_service(messages, system=None):
+Temperature:
+
+A very important concept in case of LLMs. As we know, LLMs tokenize the txt, mark them with embeddings,
+process them with a layer of attention heads and then predict the next word based on the probability distribution
+
+Now, the temperature parameter is used to adjust the probability of next word pic
+
+lower the temperature, picks the most probable word, 
+higher the temp pics, the word of more lineament choice
+Not essentially it pics the lower probability word in case of high temperature. just that it pics 
+lenient probable word
+
+use cases could be,
+if we want factual info, coding assistant, data extraction etc, we prefer lower temp.
+
+if we have tasks like summarization, grammar correction etc, we prefer medium temp
+
+if we have more creative tasks like brain storming, idea generation, or joke generation we prefer higher temp.
+
+temperature value is a decimal value ranging from 0-1 (similar to probability)
+
+Need consistent, factual responses? Use low temperature
+Want creative brainstorming? Dial up the temperature
+Somewhere in between? Medium temperatures work well for most general tasks
+
+Lecture link: https://anthropic.skilljar.com/claude-with-the-anthropic-api/287728
+'''
+
+
+def chat_service(messages, system=None, temperature=1.0):
     client = Anthropic()
     model = "claude-sonnet-4-0"
     params = {
         "model": model,
         "max_tokens": 1000,
         "messages": messages,
+        "temperature": temperature
     }
 
     if system:
