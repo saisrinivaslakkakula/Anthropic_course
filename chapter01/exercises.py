e23eecfda4cd890f032f35ded4f89a74e3eddde6
Sai Srinivas Lakkakula <saisrinu.lakkakula@gmail.com>
2025-07-14 02:20:57 -0700

    Ch01 - structure data exercise

--- chapter01/exercises.py
+++ chapter01/exercises.py
@@ -6,7 +6,7 @@ def exercise_system_prompt_and_temperature(messages):
     while True:
         user_text = input("YOU:")
         utils.add_user_message(messages, user_text)
-        ai_response = chat.chat_service(messages,None, 0.0)
+        ai_response = chat.chat_service(messages, None, 0.0)
         print(f"AI: {ai_response}")
         utils.add_assistant_message(messages, ai_response)
 
@@ -17,3 +17,15 @@ def exercise_message_streaming(messages):
     with stream as stream:
         for text in stream.text_stream:
             print(text, end="")
+
+
+def exercise_structure_data(messages):
+    prompt = """
+    Generate three different AWS CLI commands. Each should be very short.
+    """
+    utils.add_user_message(messages, prompt)
+    utils.add_assistant_message(messages,
+                                "Here are the three commands in single line without any "
+                                "comments:\n```bash")
+    response = chat.chat_service(messages, None, 0.0, ["```"])
+    print(response)
--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from chapter01 import examples as ch01_examples
 from chapter01 import exercises as ch01_exercises
 if __name__ == '__main__':
     messages = []
-    ch01_exercises.exercise_message_streaming(messages)
+    ch01_exercises.exercise_structure_data(messages)
 
 
